047f5c01ab16d9c541fbe98d44141026bc65f8ef
Takunoko <takunoko@gmail.com>
2020-03-26 23:25:34 +0900

    トップページ(http://FQDN/)にアクセスした際に、topics/index が自動的に表示されるように変更

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'topics#index'
   get 'topics/index'
   get 'topics/show/:id' => 'topics#show', as: :topics_show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
